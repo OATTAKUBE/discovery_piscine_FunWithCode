27a427f28fae2a4e5743f3277e1d5373a3752d29
OATTAKUBE <teerasak.khlaiphet@gmail.com>
2026-02-06 17:06:09 +0700

    Already

---
+++ rush/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".navbar-nav a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70 
+            }, 800, function(){
+
+            });
+        } 
+    });
+});
